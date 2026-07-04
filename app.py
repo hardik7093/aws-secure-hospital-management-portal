5acbae75faaf668e2105a39d1ab58aa806fa880b
hardik7093 <hardikmore5@gmail.com>
2026-07-04 09:33:30 +0000

    Integrated AWS Secrets Manager for secure database credentials

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
 from flask import Flask, render_template, request, redirect
 import pymysql
+import boto3
+import json
 
 app = Flask(__name__)
 
+# Get database credentials from AWS Secrets Manager
+def get_db_secret():
+    client = boto3.client(
+        "secretsmanager",
+        region_name="us-east-1"
+    )
+
+    response = client.get_secret_value(
+        SecretId="hospital-db-secret"
+    )
+
+    return json.loads(response["SecretString"])
+
+# Load secret
+secret = get_db_secret()
+
+# Create MySQL connection
 connection = pymysql.connect(
-    host="hospital-db.cqz42w2yo835.us-east-1.rds.amazonaws.com",
-    user="admin",
-    password="Hardik9998",
-    database="hospitaldb",
+    host=secret["host"],
+    user=secret["username"],
+    password=secret["password"],
+    database=secret["database"],
+    port=int(secret["port"]),
     cursorclass=pymysql.cursors.DictCursor
 )
 
@@ -21,7 +41,6 @@ def home():
 
 @app.route("/add", methods=["POST"])
 def add():
-
     name = request.form["name"]
     age = request.form["age"]
     disease = request.form["disease"]
@@ -30,7 +49,7 @@ def add():
     cursor = connection.cursor()
 
     cursor.execute(
-        "INSERT INTO patients(name,age,disease,doctor) VALUES(%s,%s,%s,%s)",
+        "INSERT INTO patients(name, age, disease, doctor) VALUES (%s, %s, %s, %s)",
         (name, age, disease, doctor)
     )
 
@@ -40,4 +59,4 @@ def add():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
