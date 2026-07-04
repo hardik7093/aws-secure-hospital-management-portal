6d2b53b7a7568bb7768f6fd076bdd1064b2ebc53
hardik7093 <hardikmore5@gmail.com>
2026-07-04 09:13:03 +0000

    Implemented patient CRUD with Flask and RDS integration

--- app.py
+++ app.py
@@ -1,14 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import pymysql
 
 app = Flask(__name__)
 
+connection = pymysql.connect(
+    host="hospital-db.cqz42w2yo835.us-east-1.rds.amazonaws.com",
+    user="admin",
+    password="Hardik9998",
+    database="hospitaldb",
+    cursorclass=pymysql.cursors.DictCursor
+)
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM patients ORDER BY id DESC")
+    patients = cursor.fetchall()
+    return render_template("dashboard.html", patients=patients)
 
-@app.route("/dashboard")
-def dashboard():
-    return render_template("dashboard.html")
+
+@app.route("/add", methods=["POST"])
+def add():
+
+    name = request.form["name"]
+    age = request.form["age"]
+    disease = request.form["disease"]
+    doctor = request.form["doctor"]
+
+    cursor = connection.cursor()
+
+    cursor.execute(
+        "INSERT INTO patients(name,age,disease,doctor) VALUES(%s,%s,%s,%s)",
+        (name, age, disease, doctor)
+    )
+
+    connection.commit()
+
+    return redirect("/")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
